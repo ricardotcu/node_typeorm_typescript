e0a77fda2d2bfbecc008bb2b19b6ed31810fe914
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-20 10:16:01 -0300

    ajuste94

--- src/entity/Categorias_Home.ts
+++ src/entity/Categorias_Home.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
-@Entity()
+@Entity('categorias_home')
 export class Categorias_Home{
     @PrimaryGeneratedColumn("uuid")
     id: number;
