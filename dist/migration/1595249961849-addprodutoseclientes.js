eb23cab5f943802c45c068d07afdb3fc3a6a4e9e
ricardotcu10@gmail.com <ricardotcu10@gmail.com>
2020-07-20 10:11:00 -0300

    ajuste93

---
+++ dist/migration/1595249961849-addprodutoseclientes.js
@@ -0,0 +1,162 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.addprodutoseclientes1595249961849 = void 0;
+
+class addprodutoseclientes1595249961849 {
+  async up(queryRunner) {
+    await queryRunner.query(`INSERT INTO "cliente" ("nome", "email", "senha", "avatar", "token") VALUES ('rick', 'rick@rick.com', 'rick', '../../assets/images/cliente.png', 'ajfjhsjhjshjshfjshdjfhsjdsjdfhs')`);
+    await queryRunner.query(`INSERT INTO "cliente" ("nome", "email", "senha", "avatar", "token") VALUES ('a', 'a@a.com', 'a', '../../assets/images/cliente.png', 'ajfjhsjhjshjshfjshdjfhsjdsjdfhs')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('sapato',
+            'sapato sapato sapato sapato',
+            '289',
+            '../../assets/images/produto1.png',
+            'sim',
+            '0fca95a0-1c7e-48b6-a849-8f7b904dd292')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('camisa',
+            'camisa camisa camisa camisa',
+            '289',
+            '../../assets/images/produto3.png',
+            'sim',
+            '49826f8a-0c89-421c-85e0-46117b3e436a')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('calca',
+            'calca calca calca calca',
+            '289',
+            '../../assets/images/produto2.png',
+            'sim',
+            'e6872e2e-68ad-44d8-983a-32aa294b0ed7')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('sapato',
+            'sapato sapato sapato sapato',
+            '289',
+            '../../assets/images/produto1.png',
+            'sim',
+            '0fca95a0-1c7e-48b6-a849-8f7b904dd292')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('camisa',
+            'camisa camisa camisa camisa',
+            '289',
+            '../../assets/images/produto3.png',
+            'sim',
+            '49826f8a-0c89-421c-85e0-46117b3e436a')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('calca',
+            'calca calca calca calca',
+            '289',
+            '../../assets/images/produto2.png',
+            'sim',
+            'e6872e2e-68ad-44d8-983a-32aa294b0ed7')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('sapato',
+            'sapato sapato sapato sapato',
+            '289',
+            '../../assets/images/produto1.png',
+            'sim',
+            '0fca95a0-1c7e-48b6-a849-8f7b904dd292')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('camisa',
+            'camisa camisa camisa camisa',
+            '289',
+            '../../assets/images/produto3.png',
+            'sim',
+            '49826f8a-0c89-421c-85e0-46117b3e436a')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('calca',
+            'calca calca calca calca',
+            '289',
+            '../../assets/images/produto2.png',
+            'sim',
+            'e6872e2e-68ad-44d8-983a-32aa294b0ed7')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('sapato',
+            'sapato sapato sapato sapato',
+            '289',
+            '../../assets/images/produto1.png',
+            'nao',
+            '0fca95a0-1c7e-48b6-a849-8f7b904dd292')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('camisa',
+            'camisa camisa camisa camisa',
+            '289',
+            '../../assets/images/produto3.png',
+            'nao',
+            '49826f8a-0c89-421c-85e0-46117b3e436a')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('calca',
+            'calca calca calca calca',
+            '289',
+            '../../assets/images/produto2.png',
+            'nao',
+            'e6872e2e-68ad-44d8-983a-32aa294b0ed7')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('sapato',
+            'sapato sapato sapato sapato',
+            '289',
+            '../../assets/images/produto1.png',
+            'nao',
+            '0fca95a0-1c7e-48b6-a849-8f7b904dd292')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('camisa',
+            'camisa camisa camisa camisa',
+            '289',
+            '../../assets/images/produto3.png',
+            'nao',
+            '49826f8a-0c89-421c-85e0-46117b3e436a')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('calca',
+            'calca calca calca calca',
+            '289',
+            '../../assets/images/produto2.png',
+            'nao',
+            'e6872e2e-68ad-44d8-983a-32aa294b0ed7')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('sapato',
+            'sapato sapato sapato sapato',
+            '289',
+            '../../assets/images/produto1.png',
+            'nao',
+            '0fca95a0-1c7e-48b6-a849-8f7b904dd292')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('camisa',
+            'camisa camisa camisa camisa',
+            '289',
+            '../../assets/images/produto3.png',
+            'nao',
+            '49826f8a-0c89-421c-85e0-46117b3e436a')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('calca',
+            'calca calca calca calca',
+            '289',
+            '../../assets/images/produto2.png',
+            'nao',
+            'e6872e2e-68ad-44d8-983a-32aa294b0ed7')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('sapato',
+            'sapato sapato sapato sapato',
+            '289',
+            '../../assets/images/produto1.png',
+            'nao',
+            '0fca95a0-1c7e-48b6-a849-8f7b904dd292')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('camisa',
+            'camisa camisa camisa camisa',
+            '289',
+            '../../assets/images/produto3.png',
+            'nao',
+            '49826f8a-0c89-421c-85e0-46117b3e436a')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('calca',
+            'calca calca calca calca',
+            '289',
+            '../../assets/images/produto2.png',
+            'nao',
+            'e6872e2e-68ad-44d8-983a-32aa294b0ed7')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('sapato',
+            'sapato sapato sapato sapato',
+            '289',
+            '../../assets/images/produto1.png',
+            'nao',
+            '0fca95a0-1c7e-48b6-a849-8f7b904dd292')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('camisa',
+            'camisa camisa camisa camisa',
+            '289',
+            '../../assets/images/produto3.png',
+            'nao',
+            '49826f8a-0c89-421c-85e0-46117b3e436a')`);
+    await queryRunner.query(`INSERT INTO "produto" ("nome", "descricao", "valor", "caminho", "destaque", "categoriaId") VALUES ('calca',
+            'calca calca calca calca',
+            '289',
+            '../../assets/images/produto2.png',
+            'nao',
+            'e6872e2e-68ad-44d8-983a-32aa294b0ed7')`);
+  }
+
+  async down(queryRunner) {}
+
+}
+
+exports.addprodutoseclientes1595249961849 = addprodutoseclientes1595249961849;
